e4540cbabbcb36d2121d0ecd7a17b413b4bcf742
Francesco <xdigrigoli@gmail.com>
2024-01-03 09:42:12 +0100

    Test2
    Mobile test2

---
+++ mobile/main.php
@@ -0,0 +1,31 @@
+<!--Questa sarà la pagina del main su mobile-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/main.css">
+    <title>CR</title>
+</head>
+<body>
+    <nav class="navigation-panel">
+
+        <div class="search">
+            <img id="search-p" src="img/search.png">
+        </div>
+
+        <div class="msg">
+            <img id="msg-p" src="img/message.png">
+        </div>
+
+        <div class="logo-bar">
+            
+        </div>
+
+        <!-- This is a dot menu  -> : -->
+        <div class="dot-menu">
+
+        </div>
+    </nav>
+</body>
+</html>
