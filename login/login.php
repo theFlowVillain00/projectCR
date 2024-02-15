932c368f58cc82bf647b0ebdef94b005f4ddc9ab
root <root@Tkcentre.localhost>
2024-02-15 12:38:28 -0600

    PisciaTina

--- login/login.php
+++ login/login.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/log-reg.css">
+    <meta name="google-signin-client_id" content="578898538060-gcjikar14vkhigmscno3lkfttsudprhi.apps.googleusercontent.com">
     <title>Login</title>
     <script src="https://apis.google.com/js/platform.js" async defer></script>
 </head>
@@ -15,17 +16,58 @@
                 <label for="username">Username:</label>
                 <input type="text" id="username" name="username" required>
 
-    <div id="form-1">
-        <form action="function/check.php" method="post">
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
 
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required>
-
-            <button type="submit">Submit</button>
-        </form>
+                <input type="hidden" name="azione" value="login">
+                <button type="submit">Accedi</button>
+            </form>
+            <div id="form-3">
+                <p1 id="string-f3">Oppure</p1>
+                <br>
+                <button type="submit" onclick="cambiaPagina()">Registrati</button>
+            </div>
+            <div class="g-signin2" data-onsuccess="onSignIn"></div>
+        </div>
     </div>
-    
+
+    <script>
+
+        function cambiaPagina() {
+            // Cambia la pagina quando viene fatto clic sul pulsante
+            window.location.href = "registrazione.php";
+        }
+
+        // Initialize the Google API client library
+        function initGoogleSignIn() {
+            gapi.load('auth2', function() {
+                gapi.auth2.init({
+                    client_id: '578898538060-gcjikar14vkhigmscno3lkfttsudprhi.apps.googleusercontent.com'
+                }).then(function () {
+                    // Create the Google Sign-In button
+                    gapi.signin2.render('google-login-button', {
+                        'scope': 'profile email',
+                        'width': 40,
+                        'height': 40,
+                        'longtitle': true,
+                        'theme': 'dark',
+                        'onsuccess': onGoogleSignIn
+                    });
+                });
+            });
+        }
+
+        // Function called when the user successfully signs in with Google
+        function onGoogleSignIn(googleUser) {
+            // Handle Google sign-in here, e.g., send data to a backend for verification
+            var profile = googleUser.getBasicProfile();
+            console.log('ID: ' + profile.getId());
+            console.log('Name: ' + profile.getName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail());
+        }
+        // Call the function to initialize Google Sign-In
+        initGoogleSignIn();
+    </script>
 </body>
 </html>
